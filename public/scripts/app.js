fa0f5b31feed38ef6ed3b94631487a65015f77dd
BryanReese <bryan_reese209@yahoo.com>
2017-09-06 15:50:40 -0700

    delete style working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
+	$.ajax({
+		method:"GET",
+		url: '/api/styles',
+		success: function(style) {
+			renderStyles(style);
+			renderListStyle(style);
+		}
+	})
 
-$('.addStyles').on('submit', addingStyle);
+	$('.addStyles').on('submit', addingStyle);
+	$('.styleDisplay').on('click', '.delete-style', deletingStyle);
 
 });
 
@@ -11,40 +20,79 @@ function addingStyle(e) {
 
 	let formData = $(this).serialize();
 	console.log(formData);
-$.ajax({
+	$.ajax({
 		method: "POST",
 		url: '/api/styles',
 		data: formData,
-		success: renderStyles
+		success: function(style) {
+			renderStyles(style);
+			renderListStyle(style);
+		}
 	})
 }
 
 
+
+
 function renderStyles(style) {
 
-let styleHtml = `
+
+	let styleHtml = `
+	<!-- Style information -->
+
+	<div class="card-header styleClass" data-styleid="${style._id}"><h1>${style.type}</h1>
+		<div class="card-body">
+			<p class="card-title">Description: ${style.description}</p>
+			<p class="card-text">Comments: ${style.comments}</p>
+			<p class="card-text"><a href="${style.link}">Wiki link</a></p>
+			<div class="row">
+				<div class="col-sm-2">
+					<button class="btn-danger delete-style" data-id="${style._id}">Delete</button>
+				</div>
+				<div class="col-sm-2">
+					<button class="btn-info">edit</button>
+				</div>
+			</div>
+		</div>
+		</div>
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Add school</button>
 	
-  <a href="#" class="list-group-item list-group-item-action">${style.type}</a>
 
-<!-- Style information -->
-<div class="card bg-light mb-3 styleDisplay">
-  <div class="card-header"><h1>${style.type}</h1></div>
-  <div class="card-body">
-    <p class="card-title">Description: ${style.description}</p>
-    <p class="card-text">Comments: ${style.comments}</p>
-    <p class="card-text"><a href="${style.link}">Wiki link</a></p>
-    <div class="row">
-    <div class="col-sm-2">
-<button class="btn-danger">Delete</button>
-</div>
-<div class="col-sm-2">
-<button class="btn-info">edit</button>
-</div>
-</div>
-  </div>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Add school</button>
-</div>
+	`
 
-`
-$('.list-group').prepend(styleHtml);
+	$('.styleDisplay').prepend(styleHtml);
 }
+
+
+
+function renderListStyle(style) {
+
+	let styleHtml=`
+	<a href="#" class="list-group-item list-group-item-action">${style.type}</a>
+	`
+	$('.list-group-style').prepend(styleHtml);
+
+}
+
+function deletingStyle(e) {
+	e.preventDefault();
+let styleId = $(this).attr("data-id");
+console.log(styleId);
+$.ajax({
+	url: '/api/styles/'+styleId,
+	method:'DELETE',
+	success: function() {
+	$('[data-styleid=' + styleId +']').remove();
+
+	},
+	error: function() {
+		console.log("did not work ");
+	}
+})
+}
+
+
+
+
+
+
