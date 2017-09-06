01a12d3e4f5470be64cd8b8240d2686be848cf59
BryanReese <bryan_reese209@yahoo.com>
2017-09-06 12:24:44 -0700

    rendering styles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
-console.log("shits workin!");
+
+
+$('.addStyles').on('submit', addingStyle);
 
 });
+
+
+function addingStyle(e) {
+	e.preventDefault();
+
+	let formData = $(this).serialize();
+	console.log(formData);
+$.ajax({
+		method: "POST",
+		url: '/api/styles',
+		data: formData,
+		success: renderStyles
+	})
+}
+
+
+function renderStyles(style) {
+
+let styleHtml = `
+	
+  <a href="#" class="list-group-item list-group-item-action">${style.type}</a>
+
+<!-- Style information -->
+<div class="card bg-light mb-3 styleDisplay">
+  <div class="card-header"><h1>${style.type}</h1></div>
+  <div class="card-body">
+    <p class="card-title">Description: ${style.description}</p>
+    <p class="card-text">Comments: ${style.comments}</p>
+    <p class="card-text"><a href="${style.link}">Wiki link</a></p>
+    <div class="row">
+    <div class="col-sm-2">
+<button class="btn-danger">Delete</button>
+</div>
+<div class="col-sm-2">
+<button class="btn-info">edit</button>
+</div>
+</div>
+  </div>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Add school</button>
+</div>
+
+`
+$('.list-group').prepend(styleHtml);
+}
